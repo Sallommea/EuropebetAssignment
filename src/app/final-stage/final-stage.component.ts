d7158d9c7056f0769f4d53ebe1cf9a9393b322c2
Sallommea <salome.chkhaidze94@gmail.com>
2023-04-24 23:45:13 +0400

    third page done

--- src/app/final-stage/final-stage.component.ts
+++ src/app/final-stage/final-stage.component.ts
@@ -73,6 +73,29 @@ export class FinalStageComponent implements OnInit {
     },
   ];
 
+  keyData = [
+    { id: '1', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '2', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '3', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '4', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '5', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '6', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '7', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '8', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '9', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '10', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '11', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '12', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '13', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '14', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '15', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '16', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '17', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '18', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '19', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+    { id: '20', prize: 'სპეციალური ტურნირი Cashgame Sharks II ადგილი' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,14 +1,32 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-  constructor() {}
+  currentUrl: string = '';
+  show = true;
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('Current URL:', this.router.url);
+    this.currentUrl = this.router.url;
+    if (this.currentUrl === '/finalstage') {
+      this.show = false;
+    }
+  }
+
+  showComponent() {
+    if (this.show === false) {
+      this.show = true;
+    }
+  }
+
+  hideComponent() {
+    this.show = false;
+  }
 
   onCloseModal() {}
 }
